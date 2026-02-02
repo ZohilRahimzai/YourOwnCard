d38e0508f4d42a97d4d44cb4d8a1204e52841ad3
Zohil Rahimzai <zohilkhan2026@gmail.com>
2026-02-02 17:04:35 +0430

    JS Updated

--- OwnCard/index.js
+++ OwnCard/index.js
@@ -8,3 +8,49 @@ function createCard() {
     let email = document.getElementById("emailInput").value;
     let github = document.getElementById("githubInput").value;
     let imageFile = document.getElementById("imageInput").files[0];
+
+    let reader = new FileReader();
+    reader.onload = function () {
+
+         let card = document.createElement("div");
+        card.id = "sdiv";
+
+        card.innerHTML = `
+            <img id="img1" src="${reader.result}">
+            <h2>${name}</h2>
+            <p>${profession}</p>
+            <p>📞 ${phone}</p>
+            <p>📧 ${email}</p>
+            <p>
+                GitHub: <a href="${github}" target="_blank">Visit</a>
+            </p>
+
+            <button class="downloadBtn" onclick="downloadCard(this)">
+                Download Card
+            </button>
+        `;
+
+        container.appendChild(card);
+    };
+
+    if (imageFile) {
+        reader.readAsDataURL(imageFile);
+    }
+
+}
+
+function downloadCard(button) {
+
+    
+    let card = button.parentElement;
+
+    
+    html2canvas(card).then(canvas => {
+
+        let link = document.createElement("a");
+        link.download = "profile-card.png";
+        link.href = canvas.toDataURL();
+
+        link.click();
+    });
+}
