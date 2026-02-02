2c6329236e745448352279d791670c13ee449de8
Zohil Rahimzai <zohilkhan2026@gmail.com>
2026-02-02 16:55:22 +0430

    Updated

---
+++ OwnCard/index.js
@@ -0,0 +1,10 @@
+let container = document.getElementById("x");
+
+function createCard() {
+
+    let name = document.getElementById("nameInput").value;
+    let profession = document.getElementById("professionInput").value;
+    let phone = document.getElementById("phoneInput").value;
+    let email = document.getElementById("emailInput").value;
+    let github = document.getElementById("githubInput").value;
+    let imageFile = document.getElementById("imageInput").files[0];
